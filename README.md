032cfabe0712cc729615bd87ebccdc30eacf99c6
0xyolo <71206358+0xyolo@users.noreply.github.com>
2021-04-09 17:46:43 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+
+
+# Install
+
+```
+yarn install
+```
+
+# Edit hardhat.config.js
+
+```js
+// hardhat.config.js
+module.exports = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    version: "0.5.17" ,
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        // fill your project id or your rpc endpoint here.
+        url: "https://mainnet.infura.io/v3/<PROJECT_ID>" 
+      }
+    }
+  },
+};
+```
+
+# Compile contract
+
+```
+npx hardhat compile
+```
+
+# Run the flashloan task
+
+```
+npx hardhat flashloan
+```
+
+You can modify `FlashLoanExample.sol` and flashloan task code in `hardhat.config.js` to
+play around flashloan feature on mainnet fork.
+Don't forget to re-compile your contract when you modify it.
---
+++ hardhat.config.js
@@ -0,0 +1,76 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require("@nomiclabs/hardhat-ethers");
+
+const ERC20ABI = [
+  'function balanceOf(address) external view returns (uint)',
+  'function transfer(address, uint) external returns (bool)',
+]
+
+// Run this task with mainnet fork,
+// so that you can see play around your flashloan example contract
+task("flashloan", async (_, hre) => {
+
+    // The address that has USDC on mainnet
+    const walletAddr = '0x6D5a7597896A703Fe8c85775B23395a48f971305'
+
+    const crUSDCAddr = '0x44fbeBd2F576670a6C33f6Fc0B00aA8c5753b322'
+    const USDCAddr = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [walletAddr]
+    });
+
+    const wallet = await hre.ethers.provider.getSigner(walletAddr);
+    const factory = await hre.ethers.getContractFactory('FlashloanExample');
+
+    // deploy flashloan example contract
+    const flashloanExampleContract = await factory.deploy();
+
+    // Send 100 USDC to flash loan example contract,
+    // so that you have enough fund to pay the fee.
+    const USDC = new ethers.Contract(USDCAddr, ERC20ABI, wallet);
+    let tx = await USDC.transfer(flashloanExampleContract.address, 100 * 1e6)
+    await tx.wait()
+
+
+    console.log('contract:', flashloanExampleContract.address);
+
+    // call the doFlashloan
+    tx = await flashloanExampleContract.doFlashloan(crUSDCAddr, 10000 * 1e6);
+    const receipt = await tx.wait()
+
+    // see the result
+    console.log(receipt.events)
+
+
+    await hre.network.provider.request({
+      method: "hardhat_stopImpersonatingAccount",
+      params: [walletAddr]
+    });
+  })
+
+
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    version: "0.5.17" ,
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://mainnet.infura.io/v3/<PROJECT_ID>"
+      }
+    }
+  },
+};
