81d4748eb3a819b9fedfbdea93b5096681bb5119
Def Paf <warren.y.cheng@gmail.com>
2021-08-30 11:56:53 +0800

    fix tasks
    fix comment
    
    remove space
    
    remove space, edit text

--- contracts/FlashloanBorrower.sol
+++ contracts/FlashloanBorrower.sol
@@ -1,16 +1,32 @@
-pragma solidity ^0.5.16;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
 
 import "./ERC3156FlashLenderInterface.sol";
 import "./ERC3156FlashBorrowerInterface.sol";
 
+interface Comptroller {
+    function isMarketListed(address cTokenAddress) external view returns (bool);
+}
+
+interface ERC20 {
+    function approve(address spender, uint256 amount) external;
+}
 // FlashloanBorrower is a simple flashloan Borrower implementation for testing
 contract FlashloanBorrower is ERC3156FlashBorrowerInterface {
+    /**
+     * @notice C.R.E.A.M. comptroller address
+     */
+    address public comptroller;
+    constructor(address _comptroller) {
+        comptroller = _comptroller;
+    }
+
     function doFlashloan(
         address flashloanLender,
         address borrowToken,
         uint256 borrowAmount
     ) external {
-        bytes memory data = abi.encode(cToken, borrowAmount);
+        bytes memory data = abi.encode(borrowToken, borrowAmount);
         ERC3156FlashLenderInterface(flashloanLender).flashLoan(this, borrowToken, borrowAmount, data);
     }
 
@@ -20,10 +36,13 @@ contract FlashloanBorrower is ERC3156FlashBorrowerInterface {
         uint256 amount,
         uint256 fee,
         bytes calldata data
-    ) external returns (bytes32) {
+    ) override external returns (bytes32) {
+        require(Comptroller(comptroller).isMarketListed(msg.sender), "untrusted message sender");
         require(initiator == address(this), "FlashBorrower: Untrusted loan initiator");
-        ERC20(borrowToken).approve(msg.sender, amount.add(fee));
-        (address cToken, uint256 borrowAmount) = abi.decode(data, (address, uint256));
+        (address borrowToken, uint256 borrowAmount) = abi.decode(data, (address, uint256));
+        require(borrowToken == token, "encoded data (borrowToken) does not match");
+        require(borrowAmount == amount, "encoded data (borrowAmount) does not match");
+        ERC20(token).approve(msg.sender, amount + fee);
         // your logic is written here...
         return keccak256("ERC3156FlashBorrowerInterface.onFlashLoan");
     }
--- hardhat.config.js
+++ hardhat.config.js
@@ -11,12 +11,16 @@ const ERC20ABI = [
 
 // Run this task with mainnet fork,
 // so that you can see play around your flashloan example contract
-task("flashloan", async (_, hre) => {
+task("flashloanV1", async (_, hre) => {
 
     // The address that has USDC on mainnet
     const walletAddr = '0xf977814e90da44bfa03b6295a0616a897441acec'
     const USDCAddr = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-    const flashloanLenderAddr = ''
+
+    // below flashLender and comptroller addresses are for C.R.E.A.M. V1
+    const flashloanLenderAddr = '0xa8682Cfd2B6c714d2190fA38863d545c7a0b73D5'
+    const comptrollerAddr = '0x3d5BC3c8d13dcB8bF317092d84783c2697AE9258'
+
 
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
@@ -27,7 +31,7 @@ task("flashloan", async (_, hre) => {
     const factory = await hre.ethers.getContractFactory('FlashloanBorrower');
 
     // deploy flashloan Borrower contract
-    const flashloanBorrowerContract = await factory.deploy();
+    const flashloanBorrowerContract = await factory.deploy(comptrollerAddr);
 
     // Send 100 USDC to flash loan Borrower contract,
     // so that you have enough fund to pay the fee.
@@ -53,11 +57,52 @@ task("flashloan", async (_, hre) => {
   })
 
 
+  task("flashloanIronBank", async (_, hre) => {
+
+    // The address that has SNX on mainnet
+    const walletAddr = '0xf977814e90da44bfa03b6295a0616a897441acec'
+    const SNXAddr = '0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F'
+
+    const flashloanLenderAddr = '0x1a21Ab52d1Ca1312232a72f4cf4389361A479829'
+    const comptrollerAddr = '0xAB1c342C7bf5Ec5F02ADEA1c2270670bCa144CbB'
+
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [walletAddr]
+    });
+
+    const wallet = await hre.ethers.provider.getSigner(walletAddr);
+    const factory = await hre.ethers.getContractFactory('FlashloanBorrower');
+
+    // deploy flashloan Borrower contract
+    const flashloanBorrowerContract = await factory.deploy(comptrollerAddr);
+
+    // Send SNX to flash loan Borrower contract,
+    // so that you have enough fund to pay the fee.
+    const SNX = new ethers.Contract(SNXAddr, ERC20ABI, wallet);
+    let tx = await SNX.transfer(flashloanBorrowerContract.address, 100 * 1e6)
+    await tx.wait()
+
+    console.log('contract:', flashloanBorrowerContract.address);
+    // call the doFlashloan
+    tx = await flashloanBorrowerContract.doFlashloan(flashloanLenderAddr, SNXAddr, 10000 * 1e6);
+    const receipt = await tx.wait()
+
+    // see the result
+    console.log(receipt.events)
+
+
+    await hre.network.provider.request({
+      method: "hardhat_stopImpersonatingAccount",
+      params: [walletAddr]
+    });
+  })
+
 
 module.exports = {
   defaultNetwork: "hardhat",
   solidity: {
-    version: "0.5.17" ,
+    version: "0.8.0" ,
     settings: {
       optimizer: {
         enabled: true,
